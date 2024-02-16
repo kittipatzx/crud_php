d4138232b304dfc5e614b282dfcf836013a7fdbb
kittipatzx <151115331+kittipatzx@users.noreply.github.com>
2024-02-16 11:29:48 +0700

    Add files via upload

--- connect_db.php
+++ connect_db.php
@@ -9,6 +9,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
